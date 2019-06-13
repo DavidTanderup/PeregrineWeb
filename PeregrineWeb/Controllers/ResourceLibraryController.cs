a9b9b83725d53e9d5bbf737d6aa70871a4040ab6
David Tanderup <Davetanderup@gmail.com>
2019-06-13 09:56:34 -0400

    Updates Resource Library

---
+++ PeregrineWeb/Controllers/ResourceLibraryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PeregrineWeb.Controllers
+{
+    public class ResourceLibraryController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "When Time and Details Matter.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact Me";
+
+            return View();
+        }
+    }
+}
