a01369004b06aab724a5d0dad4cdb9059771114e
David Tanderup <Davetanderup@gmail.com>
2019-06-17 07:36:46 -0400

    Adds open new browser window

--- PeregrineWeb/Controllers/HomeController.cs
+++ PeregrineWeb/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace PeregrineWeb.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "About Peregrine Research";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact Me";
 
             return View();
         }
